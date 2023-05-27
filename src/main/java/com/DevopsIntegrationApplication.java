4ff63753b64ae9f8df4c62040afeeaca26be6e31
ibtissame <ibtissame.oumahrir@usmba.ac.ma>
2023-05-27 15:20:07 +0100

    new change

--- src/main/java/com/DevopsIntegrationApplication.java
+++ src/main/java/com/DevopsIntegrationApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie;
+package com.ibtissameoumahrir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/DevopsIntegrationApplicationTests.java
@@ -3,9 +3,10 @@ package com.ibtissameoumahrir;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DevopsIntegrationApplicationTests {
+//@SpringBootTest(classes = DevopsIntegrationApplication.class)
 
+// @SpringBootTest class DevopsIntegrationApplicationTests {
+	@SpringBootTest(classes = DevopsIntegrationApplication.class){
 	@Test
 	void contextLoads() {
 	}
